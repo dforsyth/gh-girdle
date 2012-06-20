aa9acc031deaa10d6cce6a2f8042e8b8098d4b4f
Krishna Rajendran <krishna@emptybox.org>
2012-06-20 12:51:24 -0700

    Invoke gh_news(engirdle) on pageUpdate

--- ext/gh.js
+++ ext/gh.js
@@ -6,122 +6,133 @@
 // ==/UserScript==
 
 function gh_news() {
-    $('.news').each(function(index) {
-        var compressed = {};
-        $('.alert', this).each(function(index) {
-            var alert_type = $(this).attr('class')
-            var title_elems = $('.title', this).find('a')
+    function engirdle() {
+        $('.news').each(function(index) {
+            var compressed = {};
+            $('.alert', this).each(function(index) {
+                var alert_type = $(this).attr('class')
+                var title_elems = $('.title', this).find('a')
 
-            // grab the user
-            var user = $.trim($('.name', '#userbox').text());
+                // grab the user
+                var user = $.trim($('.name', '#userbox').text());
 
-            var repo = '';
+                var repo = '';
 
-            // don't handle git_hub for now.
-            if (alert_type == 'alert create') {
-                var key = $(title_elems).get(2);
-                if (key == undefined) {
-                    key = $(title_elems).get(0);
+                // don't handle git_hub for now.
+                if (alert_type == 'alert create') {
+                    var key = $(title_elems).get(2);
+                    if (key == undefined) {
+                        key = $(title_elems).get(0);
+                    }
+                    repo = $(key).text();
+                } else if (alert_type == 'alert gist') {
+                    var key = $(title_elems).get(0);
+                    repo = $(key).text();
+                } else if (alert_type == 'alert follow') {
+                    var key = $(title_elems).get(1);
+                    if ($(key).text() != user) {
+                        key = $(title_elems).get(0);
+                    }
+                    repo = $(key).text();
+                } else if (alert_type == 'alert issues_opened' ||
+                    alert_type == 'alert issues_closed' ||
+                    alert_type == 'alert issues_reopened') {
+                    repo = $($(title_elems).get(2)).text();
+                } else if (alert_type == 'alert push' ||
+                    alert_type == 'alert commit_comment' ||
+                    alert_type == 'alert download' ||
+                    alert_type == 'alert delete' ||
+                    alert_type == 'alert gollum' ||
+                    alert_type == 'alert fork' ||
+                    alert_type == 'alert watch_started') {
+                    repo = $($(title_elems).get(1)).text();
+                } else if (alert_type == 'alert issues_comment') {
+                    var repo_elem = $(title_elems).get(2);
+                    if (repo_elem == undefined) {
+                        // comment on a repo
+                        repo_elem = $(title_elems).get(1);
+                    }
+                    repo = $(repo_elem).text();
+                } else {
+                    console.log('unknown: ' + alert_type);
+                    repo = 'unknown';
                 }
-                repo = $(key).text();
-            } else if (alert_type == 'alert gist') {
-                var key = $(title_elems).get(0);
-                repo = $(key).text();
-            } else if (alert_type == 'alert follow') {
-                var key = $(title_elems).get(1);
-                if ($(key).text() != user) {
-                    key = $(title_elems).get(0);
+                if (!compressed[repo]) {
+                    compressed[repo] = [];
                 }
-                repo = $(key).text();
-            } else if (alert_type == 'alert issues_opened' ||
-                alert_type == 'alert issues_closed' ||
-                alert_type == 'alert issues_reopened') {
-                repo = $($(title_elems).get(2)).text();
-            } else if (alert_type == 'alert push' ||
-                alert_type == 'alert commit_comment' ||
-                alert_type == 'alert download' ||
-                alert_type == 'alert delete' ||
-                alert_type == 'alert gollum' ||
-                alert_type == 'alert fork' ||
-                alert_type == 'alert watch_started') {
-                repo = $($(title_elems).get(1)).text();
-            } else if (alert_type == 'alert issues_comment') {
-                var repo_elem = $(title_elems).get(2);
-                if (repo_elem == undefined) {
-                    // comment on a repo
-                    repo_elem = $(title_elems).get(1);
-                }
-                repo = $(repo_elem).text();
-            } else {
-                console.log('unknown: ' + alert_type);
-                repo = 'unknown';
-            }
-            if (!compressed[repo]) {
-                compressed[repo] = [];
-            }
-            compressed[repo].push($(this));
-            $(this).remove();
-        });
+                compressed[repo].push($(this));
+                $(this).remove();
+            });
 
-        for (kk in compressed) {
-            (function(k) {
-                var $gh_alert = $('<div class="alert"></div>');
+            for (kk in compressed) {
+                (function(k) {
+                    var $gh_alert = $('<div class="alert"></div>');
 
-                var $body = $('<div class="body"></div>');
+                    var $body = $('<div class="body"></div>');
 
-                $gh_alert.append($body);
+                    $gh_alert.append($body);
 
-                var title = '<div class="title"></div>';
-                var $title = $(title);
+                    var title = '<div class="title"></div>';
+                    var $title = $(title);
 
-                $body.append($title);
+                    $body.append($title);
 
-                var $name = $('<a href="' + k + '">' + k + '</a>');
+                    var $name = $('<a href="' + k + '">' + k + '</a>');
 
-                $title.append($name);
-                var event_str = ' had ' + compressed[k].length + ' event';
-                if (compressed[k].length > 1 ) {
-                    event_str += 's';
-                }
-                $title.append(event_str);
+                    $title.append($name);
+                    var event_str = ' had ' + compressed[k].length + ' event';
+                    if (compressed[k].length > 1 ) {
+                        event_str += 's';
+                    }
+                    $title.append(event_str);
 
-                var $second_title = $(title);
-                $body.append($second_title);
+                    var $second_title = $(title);
+                    $body.append($second_title);
 
-                $(compressed[k]).each(function(i, value) {
-                    var $icon = $('.mini-icon', value).clone();
-                    $icon.css({'position': 'relative', 'margin-right': '5px', 'margin-top': '5px'});
-                    $icon.attr('title', $.trim($('.title', value).text()));
-                    $second_title.append($icon);
-                });
-
-                var $expand = $('<a id="' + k + '" class="button">expand</a>');
-                $expand.css('float', 'right');
-
-                $expand.click(function() {
-                    var t = $expand.text();
                     $(compressed[k]).each(function(i, value) {
+                        var $icon = $('.mini-icon', value).clone();
+                        $icon.css({'position': 'relative', 'margin-right': '5px', 'margin-top': '5px'});
+                        $icon.attr('title', $.trim($('.title', value).text()));
+                        $second_title.append($icon);
+                    });
+
+                    var $expand = $('<a id="' + k + '" class="button">expand</a>');
+                    $expand.css('float', 'right');
+
+                    $expand.click(function() {
+                        var t = $expand.text();
+                        $(compressed[k]).each(function(i, value) {
+                            if (t == 'expand') {
+                                $(value).appendTo($body);
+                            } else {
+                                $(value).remove()
+                            }
+                        });
                         if (t == 'expand') {
-                            $(value).appendTo($body);
+                            $second_title.remove();
+                            $expand.text('compress');
                         } else {
-                            $(value).remove()
+                            $body.append($second_title);
+                            $expand.text('expand');
                         }
                     });
-                    if (t == 'expand') {
-                        $second_title.remove();
-                        $expand.text('compress');
-                    } else {
-                        $body.append($second_title);
-                        $expand.text('expand');
-                    }
-                });
 
-                $title.append($expand);
+                    $title.append($expand);
 
-                $('.news').prepend($gh_alert);
-            })(kk);
-        }
-    });
+                    $('.news').prepend($gh_alert);
+                })(kk);
+            }
+        });
+    }
+
+    engirdle();
+
+    //Intercept the pageUpdate function and have it call engirdle
+    var pageUpdate = $.fn.pageUpdate;
+    $.fn.pageUpdate = function (a) {
+        pageUpdate.call(this, a);
+        engirdle();
+    }
 }
 
 function inject(func) {
